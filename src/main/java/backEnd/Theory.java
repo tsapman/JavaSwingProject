f6c48a43c52cafd5d70c0b73b0a5a74e033e7e6e
Panagiotis Tsaparas <panostsaparas@yahoo.gr>
2023-06-29 18:03:42 +0200

    AlgebraTheory

--- src/main/java/backEnd/Theory.java
+++ src/main/java/backEnd/Theory.java
@@ -15,7 +15,25 @@ public class Theory {
                 "</p>" +
                 "    In algebra we use variables like x, y, and z along with numbers" +
                 "<p>" +
-                "" +
+                "The basic rules of algebra are the commutative rule of addition, the commutative rule of multiplication, the associative rule of" +
+                "</p>"+
+                "<p>" +
+                "addition, the associative rule of multiplication, and the distributive property of multiplication. These rules tell us the ways that addition" +
+                "</p>"+
+                "<p>" +
+                "and multiplication (and by extension, subtraction and division, respectively) behave." +
+                "</p>"+
+                "<p>" +
+                "\n\n" +
+                "</p>"+
+                "<h2 color=\"blue\">How to do Algebra</h2>"+
+                "<p><strong>Example 1:</strong> Find the value of x in the following equation using the Algebra concepts. 3x + 4 = 28</p>"+
+                "<p><strong>Solution:</strong></p>"+
+                "<p>3x + 4 = 28</p>"+
+                "<p>3x = 28 - 4</p>"+
+                "<p>3x = 24</p>"+
+                "<p>x = 8</p>"+
+                "<p>Therefore, the value of x = 8</p>"+
                 "</body>" +
                 "</html>";
 
