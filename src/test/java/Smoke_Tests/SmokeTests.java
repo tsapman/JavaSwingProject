023ef7ac903172cdccfe57d9187e5b5ae995d2ad
Panagiotis Tsaparas <panostsaparas@yahoo.gr>
2023-07-01 13:00:04 +0200

    CreatingSmokeTests

--- src/test/java/Smoke_Tests/SmokeTests.java
+++ src/test/java/Smoke_Tests/SmokeTests.java
@@ -1,26 +1,65 @@
 package Smoke_Tests;
 
 import APP.Application;
+import backEnd.NameOfTheUser;
+import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.edt.GuiActionRunner;
 import org.assertj.swing.fixture.FrameFixture;
-import org.junit.Assert;
+import org.assertj.swing.fixture.JButtonFixture;
+import org.junit.After;
 import org.junit.Test;
+
+import javax.swing.*;
+
+import static org.junit.Assert.assertTrue;
 
 //Class that hosts the Smoke tests of the Application
 public class SmokeTests {
-
+    FrameFixture frame;
+    NameOfTheUser nameOfTheUser = NameOfTheUser.getInstance();
 
     //Checking if the App starts correctly
     @Test
     public void testStartOfTheApp(){
-        FrameFixture frame;
+
         Application application = GuiActionRunner.execute(Application::new);
         frame = new FrameFixture(application.frame);
         frame.show();
 
         //Asserting if the frame of the starting page is shown correctly
-        Assert.assertTrue("The app does not start properly", frame.isEnabled());
+        assertTrue("The app does not start properly", frame.isEnabled());
 
     }
 
+    @Test
+    public void checkButtonInStartingFrame(){
+
+        Application application = GuiActionRunner.execute(Application::new);
+        frame = new FrameFixture(application.frame);
+        frame.show();
+
+        // Check the presence and behavior of a button
+        JButtonFixture buttonFixture = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of  button in starting page
+                return "Enter".equals(button.getText());
+
+            }
+        });
+
+        //check that the Enter button is on the screen
+        assertTrue("The button Enter  does not appearing", buttonFixture.isEnabled());
+
+        buttonFixture.click();
+
+
+    }
+
+    // Clean up resources
+    @After
+    public void cleanFrameAfterExecution() {
+
+        frame.cleanUp();
+    }
 }
