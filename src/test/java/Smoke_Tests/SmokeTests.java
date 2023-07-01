f3c5159401fdc3b0dc806bb4650ff6fa3754de1a
Panagiotis Tsaparas <panostsaparas@yahoo.gr>
2023-07-01 14:11:28 +0200

    CreatingSmokeTests

--- src/test/java/Smoke_Tests/SmokeTests.java
+++ src/test/java/Smoke_Tests/SmokeTests.java
@@ -56,6 +56,7 @@ public class SmokeTests {
 
     }
 
+    //Check the content of Choice menu
     @Test
     public void checkTheContentOfTheMenuOfChoicesPart1(){
         Application application = GuiActionRunner.execute(Application::new);
@@ -93,7 +94,78 @@ public class SmokeTests {
             }
         });
         assertTrue("the Geometry Theory Button is not displayed",buttonGeometryTheory.isEnabled());
+
+        JButtonFixture buttonAlgebraExercises = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of button in Choice Menu
+                return "Algebra - Exercises".equals(button.getText());
+
+            }
+        });
+        assertTrue("the Algebra - Exercises Button is not displayed",buttonAlgebraExercises.isEnabled());
     }
+
+    //Check the content of Choice menu
+    @Test
+    public void checkTheContentOfTheMenuOfChoicesPart2(){
+        Application application = GuiActionRunner.execute(Application::new);
+        frame = new FrameFixture(application.frame);
+        frame.show();
+
+        // Check the presence and behavior of a button
+        JButtonFixture buttonFixture = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of  button in starting page
+                return "Enter".equals(button.getText());
+
+            }
+        });
+
+        buttonFixture.click();
+
+        JButtonFixture buttonAlgebraTheory = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of  button in Choice Menu
+                return "Algebra - Theory".equals(button.getText());
+
+            }
+        });
+        assertTrue("the Algebra Theory Button is not displayed",buttonAlgebraTheory.isEnabled());
+
+        JButtonFixture buttonGeometryExercises = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of  button in Choice Menu
+                return "Geometry - Exercises".equals(button.getText());
+
+            }
+        });
+        assertTrue("the Geometry - Exercises Button is not displayed",buttonGeometryExercises.isEnabled());
+
+        JButtonFixture buttonMathQuiz = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of button in Choice Menu
+                return "Math Quiz".equals(button.getText());
+
+            }
+        });
+        assertTrue("the Math Quiz Button is not displayed",buttonMathQuiz.isEnabled());
+
+        JButtonFixture buttonAboutUs = frame.button(new GenericTypeMatcher<>(JButton.class) {
+            protected boolean isMatching(JButton button) {
+
+                // the text of button in Choice Menu
+                return "About Us".equals(button.getText());
+
+            }
+        });
+        assertTrue("the About Us Button is not displayed",buttonAboutUs.isEnabled());
+    }
+
 
     // Clean up resources
     @After
