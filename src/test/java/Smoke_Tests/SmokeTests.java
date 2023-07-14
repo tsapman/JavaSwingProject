9b9e663efbf903764d3efe40cae0945075342036
Panagiotis Tsaparas <panostsaparas@yahoo.gr>
2023-07-14 18:39:23 +0200

    CleanTheCode

--- src/test/java/Smoke_Tests/SmokeTests.java
+++ src/test/java/Smoke_Tests/SmokeTests.java
@@ -12,7 +12,7 @@ import javax.swing.*;
 
 import static org.junit.Assert.assertTrue;
 
-//Class that hosts the Smoke tests of the Application
+//Class that  hosts the Smoke tests of the Application
 public class SmokeTests {
     FrameFixture frame;
     NameOfTheUser nameOfTheUser = NameOfTheUser.getInstance();
